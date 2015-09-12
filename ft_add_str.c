d58bad6b65d302cc976cb7afde09f99f9f271427
Jimmy CHEN-MA <jichen-m@e3r12p21.42.fr>
2015-09-12 17:32:33 +0200

    exos

--- ft_add_str.c
+++ ft_add_str.c
@@ -1,42 +1,48 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_add_str.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jichen-m <jichen-m@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/09/12 11:07:31 by jichen-m          #+#    #+#             */
-/*   Updated: 2015/09/12 17:32:11 by jichen-m         ###   ########.fr       */
+/*   Created: 2015/09/12 14:40:02 by jichen-m          #+#    #+#             */
+/*   Updated: 2015/09/12 17:21:58 by jichen-m         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-char	*ft_add_str(char *str, int col);
-
-int		main(int argc, char **argv)
+// fonction pour remplacer les '.' par des '0'
+void	*ft_change_str(char *str)
 {
 	int		i;
-	int		col;
-	char	*str;
-	char	tab[9][9];
 
-	i = 1;
-	col = 0;
-	if (argc == 10)
+	i = 0;
+	while (str[i] != '\0')
 	{
-		while (i < argc)
+		if (str[i] == '.')
+			str[i] = '0';
+		i++;
+	}
+	return (str);
+}
+
+// fonction pour mettre le string sur le tableau int
+char	*ft_add_str(char *str, int col)
+{
+	char	*tab;
+
+	str = ft_change_str(str);
+	tab = (char *)malloc(sizeof(*str) * 82);
+	if (tab)
+	{
+		while (str[col] != '\0')
 		{
-			str = argv[i];
-//		if (ft_check_error(str) == 1)
-//			write(1, "E\n", 2);
-			tab = ft_add_str(str, col);
-			printf ("%s", tab);
-			i++;
+			tab[col] = str[col];
+			col++;
 		}
 	}
-	else
-		write(1, "E\n", 2);
+	return (tab);
 }
